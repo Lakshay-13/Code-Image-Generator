eb80a0c65c1d795f72b4de21519e1af85b3f9989
Lakshay Chawla <54232614+Lakshay-13@users.noreply.github.com>
2023-04-04 08:20:58 +0530

    Add files via upload

---
+++ Code Image Generator.py
@@ -0,0 +1,77 @@
+import streamlit as st
+from PIL import Image
+from code_to_image import convert_code_to_image
+import os
+import tempfile
+from io import BytesIO
+import base64
+
+def hex_to_rgb(hex_color):
+    hex_color = hex_color.lstrip("#")
+    return tuple(int(hex_color[i:i+2], 16) for i in (0, 2, 4))
+
+st.title("Code to Image Converter")
+
+# Input fields
+code = st.text_area("Enter your code here:")
+font_size = st.slider("Font size:", min_value=10, max_value=30, value=14, step=1)
+padding = st.slider("Padding:", min_value=5, max_value=30, value=10, step=1)
+border_width = st.slider("Border width:", min_value=1, max_value=10, value=5, step=1)
+gradient_size = st.slider("Gradient size:", min_value=1, max_value=20, value=5, step=1)
+corner_radius = st.slider("Corner radius:", min_value=0, max_value=20, value=10, step=1)
+dpi_scale= st.slider("Scaling Factor:", min_value=1, max_value=20, value=1, step=1)
+
+# Color pickers
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    text_color = st.color_picker("Text color:", value='#0AFA9E')
+with col2:
+    background_color = st.color_picker("Background color:", value='#1D1717')
+with col3:
+    border_color = st.color_picker("Gradient color:", value='#00AFEF')
+
+# Font selection dropdown
+fonts = ["Arial","Custom"]# "Times New Roman", "Terminal", "Roboto", "custom"]
+
+font_choice = st.selectbox("Font:", options=fonts)
+
+if font_choice == "Custom":
+    custom_font_file = st.file_uploader("Upload a font file (.ttf or .otf):")
+    if custom_font_file:
+        font_path = custom_font_file
+    else:
+        font_path = "arial.ttf"
+else:
+    font_path = f"{font_choice}.ttf"
+
+if st.button("Generate Image"):
+    # Call the convert_code_to_image function with user inputs
+    img = convert_code_to_image(
+        code,
+        font_path=font_path,
+        font_size=font_size,
+        background_color=hex_to_rgb(background_color),
+        text_color=hex_to_rgb(text_color),
+        padding=padding,
+        border_width=border_width,
+        border_color=hex_to_rgb(border_color),
+        gradient_size=gradient_size,
+        corner_radius=corner_radius,
+        dpi_scale=dpi_scale
+    )
+
+    # Display the generated image
+    st.image(img, caption="Generated Image", use_column_width=True)
+
+    # Save image button with styling and save functionality
+
+    def get_image_download_link(img, file_name="code_image.png", text="Save Image"):
+        buffered = BytesIO()
+        img.save(buffered, format="PNG")
+        img_str = base64.b64encode(buffered.getvalue()).decode()
+        href = f'<a href="data:image/png;base64,{img_str}" download="{file_name}" style="text-decoration:none;color:white;">{text}</a>'
+        return href
+
+    file_name = st.text_input("Enter file name:", "code_image.png")
+    st.markdown(get_image_download_link(img, file_name=file_name), unsafe_allow_html=True)
---
+++ code_to_image.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[42]:
+
+
+from PIL import Image, ImageDraw, ImageFont
+import numpy as np
+
+def convert_code_to_image(code, font_path="arial.ttf", font_size=14, background_color=(29, 23, 23), text_color=(10, 250, 158), line_height=20, padding=10, border_width=5, border_color=(255, 255, 255), gradient_size=5, corner_radius=10, dpi_scale=1):
+    
+    Image.MAX_IMAGE_PIXELS = int(4096 * 4096 * 4096 // 4 // 3)
+
+    font_size = int(font_size * dpi_scale)
+    line_height = int(line_height * dpi_scale)
+    padding = int(padding * dpi_scale)
+    border_width = int(border_width * dpi_scale)
+    gradient_size = int(gradient_size * dpi_scale)
+    corner_radius = int(corner_radius * dpi_scale)
+    
+    # font_path = "D:\Roboto\Roboto-Black.ttf"
+
+    font = ImageFont.truetype(font_path, font_size)
+
+    try:
+        font = ImageFont.truetype(font_path, font_size)
+    except OSError:
+        raise ValueError(f"Font file not found or not supported: {font_path}")
+
+    lines = code.split("\n")
+    width = max([font.getsize(line)[0] for line in lines])
+    height = len(lines) * line_height
+    image_width = width + 2 * (padding + gradient_size + border_width)
+    image_height = height + 2 * (padding + gradient_size + border_width)
+    image = Image.new("RGB", (image_width, image_height), background_color)
+    draw = ImageDraw.Draw(image)
+    y = border_width + gradient_size + padding
+    for line in lines:
+        draw.text((border_width + gradient_size + padding, y), line, text_color, font=font)
+        y += line_height
+
+    # Create gradient rectangles with rounded corners
+    gradient_rect = Image.new("RGBA", (image_width, image_height), (0, 0, 0, 0))
+    gradient_draw = ImageDraw.Draw(gradient_rect)
+
+    for i in range(gradient_size):
+        alpha = i / gradient_size
+        color = tuple([int(alpha * c) for c in border_color]) + (int(255 * alpha),)
+        gradient_draw.rounded_rectangle(
+            (border_width + i, border_width + i, image_width - 1 - (border_width + i), image_height - 1 - (border_width + i)),
+            outline=color, radius=corner_radius)
+
+    # Apply corner radius
+    gradient_rect = gradient_rect.resize((gradient_rect.width * 2, gradient_rect.height * 2), resample=Image.BOX)
+    gradient_rect = gradient_rect.crop((corner_radius, corner_radius, gradient_rect.width - corner_radius, gradient_rect.height - corner_radius))
+    gradient_rect = gradient_rect.resize((image_width, image_height), resample=Image.BOX)
+
+    # Add padding
+    padding_rect = Image.new("RGBA", (image_width, image_height), (0, 0, 0, 0))
+    padding_draw = ImageDraw.Draw(padding_rect)
+    padding_color = tuple([int((1 - 1 / gradient_size) * c) for c in border_color]) + (255,)
+    padding_draw.rounded_rectangle(
+    (border_width + gradient_size, border_width + gradient_size, image_width - 1 - (border_width + gradient_size), image_height - 1 - (border_width + gradient_size)),
+    outline=padding_color, radius=corner_radius)
+
+    # Apply corner radius to gradient_rect
+    gradient_rect = gradient_rect.resize((gradient_rect.width * 2, gradient_rect.height * 2), resample=Image.LANCZOS)
+    gradient_rect = gradient_rect.crop((corner_radius, corner_radius, gradient_rect.width - corner_radius, gradient_rect.height - corner_radius))
+    gradient_rect = gradient_rect.resize((image_width, image_height), resample=Image.LANCZOS)
+
+    # Apply corner radius to padding_rect
+    padding_rect = padding_rect.resize((padding_rect.width * 2, padding_rect.height * 2), resample=Image.LANCZOS)
+    padding_rect = padding_rect.crop((corner_radius, corner_radius, padding_rect.width - corner_radius, padding_rect.height - corner_radius))
+    padding_rect = padding_rect.resize((image_width, image_height), resample=Image.LANCZOS)
+
+    # Combine image, gradient, and padding layers
+    image = Image.alpha_composite(image.convert("RGBA"), gradient_rect)
+    image = Image.alpha_composite(image, padding_rect)
+
+    # Return final image
+    return image.convert("RGB")
+
+
+# convert_code_to_image('''from PIL import Image, ImageDraw, ImageFont
+# 
+# def convert_code_to_image(code, font_size=14, background_color=(255, 255, 255), text_color=(0, 0, 0), line_height=20):
+#     font = ImageFont.truetype("arial.ttf", font_size)
+#     lines = code.split("\n")
+#     width = max([font.getsize(line)[0] for line in lines])
+#     height = len(lines) * line_height
+#     image = Image.new("RGB", (width, height), background_color)
+#     draw = ImageDraw.Draw(image)
+#     y = 0
+#     for line in lines:
+#         draw.text((0, y), line, text_color, font=font)
+#         y += line_height
+#     return image''')
